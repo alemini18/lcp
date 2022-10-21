739d8a5d13fe674f067ad33671389a9fbc23d667
Alessandro Minisini <aleminipublic@gmail.com>
2022-10-21 22:20:46 +0200

    added new dataset

--- test.sh
+++ test.sh
@@ -1,13 +1,18 @@
 
+if [[ ! -d $1 ]]
+then
+    echo "First argument must be a directory"
+    exit 2
+fi
 
-for j in `ls dataset`
+for j in `ls $1`
 do
     echo $j
-    echo `./eps/lz77 <dataset/$j` >> $j
+    echo `./eps/lz77 < $1/$j` >> ans/$j
     for i in `cat epsilons`
     do
         echo $i
-        ./lz76 $i < dataset/$j  >> $j
+        ./lz76 $i < $1/$j  >> ans/$j
         echo $i >> $j
     done
 done
